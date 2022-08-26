82f9a1ad1670c1212fcd262496d472c2079e6b09
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-08-26 13:57:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 function submitData(name, email) {
-  const inputForm = document.querySelector("form");
-  inputForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const userInput = document.getElementById("searchByID");
-
-    fetch(`http://localhost:3000/movies/${userInput.value}`)
-    .then((response) => response.json())
-      .then((data) => {
-        const title = document.querySelector("section#movieDetails h4");
-        const summary = document.querySelector("section#movieDetails p");
-        title.innerText = data.title;
-        summary.innerText = data.summary;
-      });
-  })
-};
-
-document.addEventListener("DOMContentLoaded", init);
- 
+    let info = {name, email}
+    return fetch( "http://localhost:3000/users",
+        {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(info)
+      })
+      .then(response=>response.json()
+      )
+      .then(data=> {
+          document.body.innerHTML = data.id
+      })
+      .catch( error => {document.body.innerHTML = error.message});
+  }
